5e3808a7c228ea7685bcc594f45a8c4275f95e87
Nate Kleven <nate.kleven@gmail.com>
2018-11-09 19:37:13 -0800

    Update LED_Blink.ino
    update

--- LED_Blink/LED_Blink.ino
+++ LED_Blink/LED_Blink.ino
@@ -5,7 +5,7 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
